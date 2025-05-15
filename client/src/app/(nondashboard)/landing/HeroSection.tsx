60147e1cc93e79e296a92788d8c8f7dbd8e5599b
Zhihui Huang <zhh399@mail.usask.ca>
2025-05-15 16:08:45 -0600

    add lading page and navbar to home page. add toaster in root layout

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -42,10 +42,7 @@ const HeroSection = () => {
               placeholder="Search by city, neighborhood or address"
               className="w-full max-w-lg rounded-none rounded-l-xl border-none bg-white h-12"
             />
-            <Button
-              onClick={}
-              className="bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12"
-            >
+            <Button className="bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12">
               Search
             </Button>
           </div>
